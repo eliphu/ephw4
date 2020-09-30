ffe7bce218a1d1705b757f426274932018837509
eliz <elizabeth.phung@student.csulb.edu>
2020-09-29 18:33:51 -0700

    v2

--- ex1.js
+++ ex1.js
@@ -2,9 +2,9 @@ function myops(){
     num1 = Number(prompt("Please enter number 1"));
     num2 = Number(prompt("Please enter number 2"));
     ops = prompt("Enter the operation");
-
+    add = num1+num2
     if (ops == "+") {
-        return num1 + " + " + num2 + " = " + num1+num2;
+        return num1 + " + " + num2 + " = " + add;
     }
     else if(ops == "-"){
         return num1 + " - " + num2 + " = " + num1-num2;
